7511ea4077ef9e8a5f1d25841b59470c04fa57d0
Andrew Njoo <adnjoo@gmail.com>
2021-07-22 18:26:12 -0700

    add react-helmet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css'
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Helmet } from 'react-helmet'
 
 import Mynavbar from "./components/mynavbar.component";
 import Movielist from "./components/movielist.component";
