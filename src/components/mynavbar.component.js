940b42f2b8f010108df04b2438b82f34b218fa0c
Andrew Njoo <adnjoo@gmail.com>
2021-08-04 22:23:17 -0700

    edit

--- src/components/mynavbar.component.js
+++ src/components/mynavbar.component.js
@@ -8,9 +8,9 @@ export default class Mynavbar extends Component {
       <div>
         <Navbar bg="primary" variant="dark">
           <Container>
-            <Navbar.Brand href="/">Movies</Navbar.Brand>
+            <Navbar.Brand href="#">Movies</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#">Home</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
